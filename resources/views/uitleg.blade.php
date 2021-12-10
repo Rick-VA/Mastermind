7d318dcd331a5b3448fd7dd0ad3f8ed0690517c5
Student Justin Brinkman <jbrinkma0318@student.landstede.nl>
2021-12-10 13:48:51 +0100

    versie10

--- resources/views/uitleg.blade.php
+++ resources/views/uitleg.blade.php
@@ -2,10 +2,16 @@
 
 @section('body')
 
+<div class="uitleg2">
+    Uitleg
+</div>
+
+
 
 <div class="uitleg">
-    De speler moet de juiste kleurencode raden. . De speler kan beginnen met raden van de juiste kleuren code. In het begin zal dit random zijn. Als de kleur en plek goed is geraden zie je een rood pinnetje de plek hiervan is niet van belang. Een wit pinnetje staat voor als je alleen de kleur goed geraden hebt.
+    De speler moet de juiste kleurencode raden. . De speler kan beginnen met het raden van de juiste kleuren code. In het begin zal dit allemaal door elkaar zijn. Als de kleur en plek goed is geraden zie je een rood pinnetje de plek hiervan is niet van belang. Een wit pinnetje staat voor als je alleen de kleur goed geraden hebt.
 </div>
+
 
 
 @endsection
