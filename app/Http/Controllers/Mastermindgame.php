8eda35ed2e8908e780010a1006fead8e3b5e1887
Student Rick van Assen <rassen0134@student.landstede.nl>
2022-01-20 09:41:43 +0100

    .

--- app/Http/Controllers/Mastermindgame.php
+++ app/Http/Controllers/Mastermindgame.php
@@ -6,44 +6,8 @@ use Illuminate\Http\Request;
 
 class Mastermindgame extends Controller
 {
-    public function layout()
+    public function mmind(Request $request)
     {
-        // begin of the array
-        $speelbord = [];
-
-        for ($aantal = 0; $aantal < 10; $aantal++) {
-
-            // array with 4 items
-            $speelbord[$aantal] = [null, null, null, null];
-        }
-
-        $speelbord[0][0] = rand(0,4);
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-        echo '<table>';
-            foreach ($speelbord as $row){
-                echo'<tr>';
-                foreach($row as $position){
-                    echo'<td>';
-                    if ($position === 'null'){
-                        echo "Fout";
-                    }
-                    else{
-                        echo "Goed";
-                    }
-                    echo '</td>';
-
-                }
-                echo '</tr>';
-            }
-        echo '</table>';
         
-
-=======
->>>>>>> 49c107493d2e268a81e204b3c80211b37485f473
-=======
->>>>>>> 49c107493d2e268a81e204b3c80211b37485f473
-            return view('game')->with(['speelbord' => $speelbord]);
     }
 }
