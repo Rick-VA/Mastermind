2a1feb2433352feb4df1136441539d47aa6b0faf
Student Justin Brinkman <jbrinkma0318@student.landstede.nl>
2022-01-20 10:02:55 +0100

    nieuwe push

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -2,6 +2,15 @@
 
 @section('body')
 
+    <form method="POST" action="/game">
+        @csrf
+        <input type="submit" value="Submit">
+        <input type="text" id="color1" name="color1"> <br>
+        <input type="text" id="color2" name="color2"> <br>
+        <input type="text" id="color3" name="color3"> <br>
+        <input type="text" id="color4" name="color4"> <br>
+    </form>
+
 
 
 
